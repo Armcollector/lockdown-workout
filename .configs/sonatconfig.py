8d9c4ca29f230d859d7e5be7efea2a71ca533fa8
Christian Sloper <christian.sloper@gmail.com>
2020-12-02 23:55:34 +0100

    config

---
+++ .configs/sonatconfig.py
@@ -0,0 +1,21 @@
+import os
+import urllib.parse
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+if "DB_CONNECTIONSTRING" in os.environ:
+    params = urllib.parse.quote_plus(os.environ.get("DB_CONNECTIONSTRING"))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "iR33OXoRSUj5"
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect={}".format(params)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    VERSION = "2.1.0"
+    WTF_CSR_ENABLED = True
+    CACHE_TYPE = "simple"
+    CACHE_DEFAULT_TIMEOUT = 50
+    MAINTITLE = "Sonats Lockdown Workout "
+    INSTANCE = "SONAT"
